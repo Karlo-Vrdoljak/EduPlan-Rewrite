46dc13f9051f301f4a61b17526b892710940a8bf
KarloV <1karlo.vrdoljak@gmail.com>
2019-09-19 14:58:16 +0200

    Dodan interface Prosjeka, triba dovrsit

---
+++ EduPlan_rewrite/src/app/demo/domain/prosjeci.ts
@@ -0,0 +1,6 @@
+export interface ProsjekGodine {
+    _sumEctsUpisano;
+    _avgECTS;
+    _sumEctsPolozeno;
+    _avgOcjena;
+}
--- EduPlan_rewrite/src/app/student-prosjeci/student-prosjeci.component.ts
+++ EduPlan_rewrite/src/app/student-prosjeci/student-prosjeci.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { MenuItem } from "primeng/primeng";
 import { SubjectService } from "../demo/service/subjectService";
 import { Subject } from "../demo/domain/subject";
+import { ProsjekGodine } from "../demo/domain/prosjeci";
 
 @Component({
     selector: "app-student-prosjeci",
@@ -11,20 +12,93 @@ import { Subject } from "../demo/domain/subject";
 export class StudentProsjeciComponent implements OnInit {
     subjects: Subject[];
     cols: any[];
-    groupSubjects:any;
-    godine:number[];
+    _groupSubjects: ProsjekGodine[];
+    godine: number[];
+    rowGroupData: any;
     constructor(private subjectService: SubjectService) {}
-
+    //suma ECTS polozeno, Prosjek ocjene, broj upisanih ects, prosjek polozenih ects/god
     ngOnInit() {
-      this.subjectService
-          .getSubjects()
-          .then(subjects => (this.subjects = subjects))
-          .then(subjects => (this.godine = subjects.map(e => e.godina))
-          );
-      this.godine.filter((val,id,self) => { return self.indexOf(val) === id; })
-      this.godine.forEach(e => {
-        console.log(e);
+        this.subjectService
+            .getSubjects()
+            .then(subjects => (this.subjects = subjects));
+    }
+
+  
+
+    onSort() {
+        this.groupByYearForTable();
+    }
+
+    groupByYear() {
+      let groups = [];
+      this.subjects.forEach(function(item) {
+          var list = groups[item.godina];
+
+          if (list) {
+              list.push(item);
+          } else {
+              groups[item.godina] = [item];
+          }
       });
-      this.groupSubjects = this.subjects.filter(s => s.godina)
+      return groups;
+    }
+//polozenDaNe
+
+    filterData() {
+      //var allEcts = this.subjects.map(e => e.ects);
+      this._groupSubjects = [];
+        this.groupByYear().forEach(e => {
+          if(e){
+            let result = {} as ProsjekGodine;
+            let countPolozenoEcts = 0;
+            let countPolozenoPredmeta = 0;
+            let sumOcjena = 0;
+            
+            let sumEcts = e.map(e => e.ects).reduce((a,b) => parseInt(a) + parseInt(b),0);
+            
+            e.map(function (e) {
+              countPolozenoEcts += e.polozenDaNe == "Da" ? parseInt(e.ects) : 0 ;          
+            });
+            
+            e.map(function(e) {
+              sumOcjena += e.polozenDaNe == "Da" ? (parseInt(e.ocjena)) : 0;
+            });
+
+            e.map(function(e) {
+              countPolozenoPredmeta += e.polozenDaNe == "Da" ? 1 : 0; 
+            });
+            
+            result._sumEctsUpisano = sumEcts;
+            result._avgECTS = (countPolozenoEcts / sumEcts) * 100;
+            result._sumEctsPolozeno = countPolozenoEcts;
+            result._avgOcjena = (sumOcjena / countPolozenoPredmeta);
+            this._groupSubjects.push(result);
+
+          }
+        });
+       console.log(this._groupSubjects);        
+      }
+
+    groupByYearForTable() {
+        this.rowGroupData = {};
+        if (this.subjects) {
+            for (let i = 0; i < this.subjects.length; i++) {
+                let rowData = this.subjects[i];
+                let godina = rowData.godina;
+                if (i == 0) {
+                    this.rowGroupData[godina] = { index: 0, size: 1 };
+                } else {
+                    let previousRowData = this.subjects[i - 1];
+                    let previousRowGroup = previousRowData.godina;
+                    if (godina === previousRowGroup) {
+                        this.rowGroupData[godina].size++;
+                    } else {
+                        this.rowGroupData[godina] = { index: i, size: 1 };
+                    }
+                }
+            }
+            console.log(this.rowGroupData);
+            this.filterData();
+        }
     }
 }
