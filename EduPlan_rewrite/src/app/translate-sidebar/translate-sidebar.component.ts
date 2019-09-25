0e9cd975ca82bd4feab56c1c472e3d2159e00972
Filip Bikic <bikic111222@gmail.com>
2019-09-25 11:18:34 +0200

    Prijevod

--- EduPlan_rewrite/src/app/translate-sidebar/translate-sidebar.component.ts
+++ EduPlan_rewrite/src/app/translate-sidebar/translate-sidebar.component.ts
@@ -1,15 +1,24 @@
-import { OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-import { Location } from "@angular/common";
+import { OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 import { TranslateService } from '@ngx-translate/core';
+import { Component } from '@angular/core';
+
+//Ovo triba bit jedan .ts file, ne cila komponenta, to triba prominit
+@Component ({
+    selector: 'app-translate-sidebar',
+    templateUrl: './translate-sidebar.component.html',
+    styleUrls: ['./translate-sidebar.component.css']
+})
 
 export class TranslateSidebarComponent implements OnInit {
+
     constructor(
         private _route: ActivatedRoute,
         private _location: Location,
         private translate: TranslateService
     ) {
-        let lang = this._route.snapshot.paramMap.get("lang");
+        const lang = this._route.snapshot.paramMap.get('lang');
         this.translate.use(lang);
         this._location.back();
 
