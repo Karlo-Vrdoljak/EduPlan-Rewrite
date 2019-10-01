15aa66ee7324379f24a5e651567e2497ce27414b
KarloV <1karlo.vrdoljak@gmail.com>
2019-10-01 16:03:32 +0200

    Revert "Landing page W.I.P."
    This reverts commit 9271d482b6c82d981ad14b5afa1f82cb39baf83b.

--- EduPlan_rewrite/src/app/demo/view/emptydemo.component.ts
+++ EduPlan_rewrite/src/app/demo/view/emptydemo.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {GalleriaModule} from 'primeng/galleria';
 
 @Component({
     templateUrl: './emptydemo.component.html',
@@ -8,15 +7,8 @@ import {GalleriaModule} from 'primeng/galleria';
 
 export class EmptyDemoComponent implements OnInit {
 
-    images: any[];
-    
+
     constructor() {}
 
-    ngOnInit() {
-        this.images = [];
-        this.images.push({source:'assets/demo/images/landing/1.jpg', alt:'Description for Image 1', title:'Title 1'});
-        this.images.push({source:'assets/demo/images/landing/2.jpg', alt:'Description for Image 2', title:'Title 2'});
-        this.images.push({source:'assets/demo/images/landing/3.jpg', alt:'Description for Image 3', title:'Title 3'});
-
-     }
+    ngOnInit() { }
 }
