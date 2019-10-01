9271d482b6c82d981ad14b5afa1f82cb39baf83b
KarloV <1karlo.vrdoljak@gmail.com>
2019-10-01 09:44:59 +0200

    Landing page W.I.P.

--- EduPlan_rewrite/src/app/demo/view/emptydemo.component.ts
+++ EduPlan_rewrite/src/app/demo/view/emptydemo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {GalleriaModule} from 'primeng/galleria';
 
 @Component({
     templateUrl: './emptydemo.component.html',
@@ -7,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
 
 export class EmptyDemoComponent implements OnInit {
 
-
+    images: any[];
+    
     constructor() {}
 
-    ngOnInit() { }
+    ngOnInit() {
+        this.images = [];
+        this.images.push({source:'assets/demo/images/landing/1.jpg', alt:'Description for Image 1', title:'Title 1'});
+        this.images.push({source:'assets/demo/images/landing/2.jpg', alt:'Description for Image 2', title:'Title 2'});
+        this.images.push({source:'assets/demo/images/landing/3.jpg', alt:'Description for Image 3', title:'Title 3'});
+
+     }
 }
