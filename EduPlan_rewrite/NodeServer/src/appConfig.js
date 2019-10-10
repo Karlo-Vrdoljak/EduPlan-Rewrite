27a2852179df02020ae6b23cc08dd99e71672cbb
Filip Bikic <bikic111222@gmail.com>
2019-10-10 13:58:50 +0200

    appConfig
    Promjenjena konfiguracija baze jer se sada spajamo na cloud

--- EduPlan_rewrite/NodeServer/src/appConfig.js
+++ EduPlan_rewrite/NodeServer/src/appConfig.js
@@ -2,12 +2,15 @@ var appConfig = {};
 
 appConfig.checkAuthHeader = false; // true - provjera logina ukljucena, false - provjera logina iskljucena
 appConfig.applicationPort = 8090;
-appConfig.databaseServer = "lamasqlrazvoj";
-appConfig.instanceName = "MSSQLSERVER";
-appConfig.databaseName = "EduPlanRazvoj";
-appConfig.username = "eduplannew";
-appConfig.password = "eduplan";
+appConfig.databaseServer = "cloudvm2.westeurope.cloudapp.azure.com";
+//appConfig.instanceName = "\MSSQLSERVER";
+appConfig.databaseName = "EduPlan_Turnusno";
+appConfig.username = "sa";
+appConfig.password = "Glupan2000";
 appConfig.appVerzija = '0.9';
 appConfig.uploadPath = '../uploads/';
+options: {
+    encrypt: true
+}
 
 module.exports = appConfig;
