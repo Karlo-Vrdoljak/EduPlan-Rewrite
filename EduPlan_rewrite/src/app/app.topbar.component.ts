3f7780cb080a67f4a92c09fc0a8faa628a01d281
Filip Bikic <bikic111222@gmail.com>
2019-10-16 13:34:29 +0200

    Dodan globalni dropbox za ak.godinu i napisani potrebni servisi/apiji za njega

--- EduPlan_rewrite/src/app/app.topbar.component.ts
+++ EduPlan_rewrite/src/app/app.topbar.component.ts
@@ -1,5 +1,8 @@
 import {Component, Inject, forwardRef} from '@angular/core';
 import {AppComponent} from './app.component';
+import { StudentiService } from './_services/studenti.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { AppVariables } from './_interfaces/_configAppVariables';
 
 @Component({
     selector: 'app-topbar',
@@ -7,7 +10,31 @@ import {AppComponent} from './app.component';
 })
 export class AppTopBarComponent {
 
-    constructor(public app: AppComponent) {}
+    akademskeGodine: any;
+    akademskaGodinaSelected: any;
+    studentPodaciNaAkGodinama: any;
 
+    constructor(public app: AppComponent, private studentiService: StudentiService, private appVariables: AppVariables) {}
+
+    ngOnInit() {
+
+        this.studentiService.getAkademskeGodine().subscribe((data) => {
+          this.akademskeGodine = data;
+          this.akademskaGodinaSelected = data[0];
+          this.appVariables.PkSkolskaGodina = this.akademskaGodinaSelected.PkSkolskaGodina;
+        },
+    
+        (err: HttpErrorResponse) => {
+          if (err.error instanceof Error) {
+            console.log('Client-side error occured.');
+          } else {
+            console.log('Server-side error occured.');
+          }
+        }, () => { });
+      }
+
+      setPkSkolskaGodina() {
+        this.appVariables.PkSkolskaGodina = this.akademskaGodinaSelected.PkSkolskaGodina;
+      }
 
 }
